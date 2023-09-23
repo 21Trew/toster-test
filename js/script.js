a9e75cca83c7b821f75238ced3bef81338197c88
21Trew <21trew@gmail.com>
2023-09-24 02:33:41 +0300

    Тестовое задание для Toster media

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// Countdown
+
+let timeInSecs;
+let ticker;
+
+function startTimer(secs) {
+  timeInSecs = parseInt(secs);
+  ticker = setInterval("tick()", 1000);
+}
+
+function tick() {
+  let secs = timeInSecs;
+  if (secs > 0) {
+    timeInSecs--;
+  } else {
+    clearInterval(ticker);
+    startTimer(17476); // 5 minutes in seconds
+
+  }
+
+  let mins = Math.floor(secs / 60);
+  secs %= 60;
+  
+  let hours = Math.floor(mins / 60);
+  mins %= 60;
+
+  let result =
+    (hours < 10 ? "0" : "") + hours + ":" + (mins < 10 ? "0" : "") + mins + ":" + (secs < 10 ? "0" : "") + secs;
+
+  document.getElementById("countdown").innerHTML = result;
+}
+startTimer(17476); // 5 minutes in seconds
+
+
+// Slider
+
+$('.slider-for').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 8000,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    focusOnSelect: true,
+  });
+
+  // Droplist
+
+  var droplist = document.getElementsByClassName("droplist-button");
+  var i;
+  
+  for (i = 0; i < droplist.length; i++) {
+    droplist[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
